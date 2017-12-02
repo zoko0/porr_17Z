4cfdbcc517dd4741f2b621e58ef9f7ccecb8800f
Bartosz <bartosz.ciulkin@gmail.com>
2017-12-02 17:39:31 +0100

    fixed untracked files

--- build_and_execute.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp chapel/chapel chpl -o execute main_loop.chpl
-sudo docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp chapel/chapel ./execute
-sudo rm execute
--- conf_chpl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export CHPL_HOME=~/Pulpit/magisterka_sem3/porr/projekt/chapel-master
-export CHPL_HOST_PLATFORM="$CHPL_HOME/util/chplenv/chpl_platform.py"
-export PATH="$PATH":"$CHPL_HOME/bin/$CHPL_HOST_PLATFORM"
-export MANPATH="$MANPATH":"$CHPL_HOME"/man"
